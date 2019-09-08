f73e49f7009f0aff531ad33d5f13a4b3e0a6b004
Sim Yong Seng <simyseng@gmail.com>
2019-09-08 22:14:08 +0700

    First commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Sim Yong Seng on 22/8/19.
+//  Copyright © 2019 Sim Yong Seng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func printButtonTapped(_ sender: Any) {
+        print("Hello World!")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
